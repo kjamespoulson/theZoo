08d54d4a64ff9f0ec8c7fdc5cc2ff55df53a51c3
Jroneel <oneelj@oregonstate.edu>
2023-08-02 17:41:59 -0700

    database file and database connector added added database connector in database. The database login information should be added and then this file should be added to .gitignore.

--- app.js
+++ app.js
@@ -5,9 +5,12 @@
 */
 var express = require('express');   // We are using the express library for the web server
 var app     = express();            // We need to instantiate an express object to interact with the server in our code
+// Database
+var db = require('./database/db-connector')
 PORT        = 9124;                 // Set a port number at the top so it's easy to change in the future
 
 app.use(express.static('public'))
+
 /*
     ROUTES
 */
